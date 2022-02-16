01fc6616ae076b221e57d35d371609565eb36496
alinedantas <alinepinheiro20@gmail.com>
2022-02-15 21:38:47 -0300

    Update form.py

--- form.py
+++ form.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
-import csv
+import streamlit as st
+from google.cloud import firestore
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("thekey.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("inscritos").document("inscricao")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.nome)
+st.write("The contents are: ", doc.telefone())
+
+
 
 st.title('Registro de amigos do Professor Ricardo Dantas')
 with st.form("my_form"):
