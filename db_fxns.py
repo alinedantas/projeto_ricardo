9e53b9640db3b817ac9d023490fd346bcc9ab02d
alinedantas <alinepinheiro20@gmail.com>
2022-02-16 18:53:10 -0300

    Add files via upload

--- db_fxns.py
+++ db_fxns.py
@@ -1,82 +1,37 @@
+# DB
 import sqlite3
-
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
-
+# Functions
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT,title TEXT,article TEXT,postdate,DATE)')
+	c.execute('CREATE TABLE IF NOT EXISTS inscricao(nome TEXT,telefone TEXT,nascimento DATE,endereco TEXT,regiao TEXT,cep TEXT,email TEXT,facebook TEXT,instagram TEXT,sugestao TEXT)')
 
-
-def add_data(author, title, article, postdate):
-    c.execute('INSERT INTO blogtable(author,title,article,postdate) VALUES (?,?,?,?)',
-              (author, title, article, postdate))
-    conn.commit()
-
+def add_data(nome,telefone,nascimento,endereco,regiao,cep,email,facebook,instagram,sugestao):
+	c.execute('INSERT INTO inscricao(nome,telefone,nascimento,endereco,regiao,cep,email,facebook,instagram,sugestao) VALUES (?,?,?,?,?,?,?,?,?,?)',(nome,telefone,nascimento,endereco,regiao,cep,email,facebook,instagram,sugestao))
+	conn.commit()
 
 def view_all_notes():
-    c.execute('SELECT * FROM blogtable')
-    data = c.fetchall()
-    # for row in data:
-    # 	print(row)
-    return data
-
+	c.execute('SELECT * FROM blogtable')
+	data = c.fetchall()
+	return data
 
 def view_all_titles():
-    c.execute('SELECT DISTINCT title FROM blogtable')
-    data = c.fetchall()
-    # for row in data:
-    # 	print(row)
-    return data
-
-
-def get_single_blog(title):
-    c.execute('SELECT * FROM blogtable WHERE title="{}"'.format(title))
-    data = c.fetchall()
-    return data
+	c.execute('SELECT DISTINCT title FROM blogtable')
+	data = c.fetchall()
+	return data
 
 
 def get_blog_by_title(title):
-    c.execute('SELECT * FROM blogtable WHERE title="{}"'.format(title))
-    data = c.fetchall()
-    return data
-
+	c.execute('SELECT * FROM inscricao WHERE nome="{}"'.format(nome))
+	data = c.fetchall()
+	return data
 
 def get_blog_by_author(author):
-    c.execute('SELECT * FROM blogtable WHERE author="{}"'.format(author))
-    data = c.fetchall()
-    return data
-
-
-def get_blog_by_msg(article):
-    c.execute("SELECT * FROM blogtable WHERE article like '%{}%'".format(article))
-    data = c.fetchall()
-    return data
-
-
-def edit_blog_author(author, new_author):
-    c.execute('UPDATE blogtable SET author ="{}" WHERE author="{}"'.format(new_author, author))
-    conn.commit()
-    data = c.fetchall()
-    return data
-
-
-def edit_blog_title(title, new_title):
-    c.execute('UPDATE blogtable SET title ="{}" WHERE title="{}"'.format(new_title, title
-                                                                         ))
-    conn.commit()
-    data = c.fetchall()
-    return data
-
-
-def edit_blog_article(article, new_article):
-    c.execute('UPDATE blogtable SET title ="{}" WHERE title="{}"'.format(new_article, article
-                                                                         ))
-    conn.commit()
-    data = c.fetchall()
-    return data
-
+	c.execute('SELECT * FROM blogtable WHERE author="{}"'.format(author))
+	data = c.fetchall()
+	return data
 
 def delete_data(title):
-    c.execute('DELETE FROM blogtable WHERE title="{}"'.format(title))
-    conn.commit()
+	c.execute('DELETE FROM blogtable WHERE title="{}"'.format(title))
+	conn.commit()
--- form.py
+++ form.py
@@ -10,7 +10,6 @@ import pandas as pd
 from db_fxns import *
 
 
-
 # Reading Time
 def readingTime(mytext):
     total_words = len([token for token in mytext.split(" ")])
@@ -68,114 +67,62 @@ def main():
 		<h1 style="color:{};text-align:center;">Simple Blog </h1>
 		</div>
 		"""
-    st.markdown(html_temp.format('royalblue', 'white'), unsafe_allow_html=True)
+    #st.markdown(html_temp.format('royalblue', 'white'), unsafe_allow_html=True)
 
-    menu = ["Home", "View Post", "Add Post", "Search", "Manage Blog"]
-    choice = st.sidebar.selectbox("Menu", menu)
+    create_table()
+    st.title('Registro de amigos do Professor Ricardo Dantas')
 
-    if choice == "Home":
-        st.subheader("Home")
-        result = view_all_notes()
-        for i in result:
-            # short_article = str(i[2])[0:int(len(i[2])/2)]
-            short_article = str(i[2])[0:50]
-            st.write(title_temp.format(i[1], i[0], short_article), unsafe_allow_html=True)
+    nome = st.text_input('Nome Completo', max_chars=100, placeholder='Nome Completo')
+    telefone = st.text_input('Celular', max_chars=8, placeholder='(dd)*****-****')
+    nascimento = st.date_input('Data de Nascimento ')
+    endereco = st.text_input('Endereço', max_chars=100, placeholder='Endereço')
 
-    # st.write(result)
-    elif choice == "View Post":
-        st.subheader("View Post")
+    regiao = st.selectbox('Região em que mora no DF',
+                          ['Águas Claras ',
+                           'Arniqueira ',
+                           'Brazlândia ',
+                           'Candangolândia ',
+                           'Ceilândia ',
+                           'Cruzeiro ',
+                           'Fercal ',
+                           'Gama ',
+                           'Guará ',
+                           'Itapoã ',
+                           'Jardim Botânico ',
+                           'Lago Norte ',
+                           'Lago Sul',
+                           'Núcleo Bandeirante ',
+                           'Paranoá ',
+                           'Park Way ',
+                           'Planaltina ',
+                           'Plano Piloto ',
+                           'Recanto das Emas ',
+                           'Riacho Fundo ',
+                           'Riacho Fundo II ',
+                           'Samambaia ',
+                           'Santa Maria ',
+                           'São Sebastião ',
+                           'SCIA/Estrutural ',
+                           'SIA ',
+                           'Sobradinho ',
+                           'Sobradinho II ',
+                           'Sol Nascente e Pôr do Sol ',
+                           'Sudoeste/Octogonal ',
+                           'Taguatinga ',
+                           'Varjão ',
+                           'Vicente Pires',
+                           'Não moro no DF'])
 
-        all_titles = [i[0] for i in view_all_titles()]
-        postlist = st.sidebar.selectbox("Posts", all_titles)
-        post_result = get_blog_by_title(postlist)
-        for i in post_result:
-            st.text("Reading Time:{} minutes".format(readingTime(str(i[2]))))
-            st.markdown(head_message_temp.format(i[1], i[0], i[3]), unsafe_allow_html=True)
-            st.markdown(full_message_temp.format(i[2]), unsafe_allow_html=True)
+    cep = st.text_input('CEP', max_chars=8, placeholder='CEP')
+    email = st.text_input('E-mail', max_chars=100, placeholder='exemplo@exemplo.com')
+    facebook = st.text_input('Facebook', max_chars=100, placeholder='https://www.facebook.com/usuario')
+    instagram = st.text_input('Instagram', max_chars=100, placeholder='https://www.instagram.com/usuario/')
+    sugestao = st.text_area("Sugestão", height=200)
 
-        # if st.button("Analyze"):
+    if st.button("Enviar"):
+        add_data(nome,telefone,nascimento,endereco,regiao,cep,email,facebook,instagram,sugestao)
+        st.success("Inscrição::' de {}' Enviada".format(nome))
 
-        # 	docx = analyze_text(i[2])
-        # 	html = displacy.render(docx,style="ent")
-        # 	html = html.replace("\n\n","\n")
-        # 	st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
-
-
-
-
-    elif choice == "Add Post":
-        st.subheader("Add Your Article")
-        create_table()
-        blog_title = st.text_input('Enter Post Title')
-        blog_author = st.text_input("Enter Author Name", max_chars=50)
-        blog_article = st.text_area("Enter Your Message", height=200)
-        blog_post_date = st.date_input("Post Date")
-        if st.button("Add"):
-            add_data(blog_author, blog_title, blog_article, blog_post_date)
-            st.success("Post::'{}' Saved".format(blog_title))
-
-
-    elif choice == "Search":
-        st.subheader("Search Articles")
-        search_term = st.text_input("Enter Term")
-        search_choice = st.radio("Field to Search", ("title", "author"))
-        if st.button('Search'):
-            if search_choice == "title":
-                article_result = get_blog_by_title(search_term)
-            elif search_choice == "author":
-                article_result = get_blog_by_author(search_term)
-
-            # Preview Articles
-            for i in article_result:
-                st.text("Reading Time:{} minutes".format(readingTime(str(i[2]))))
-                # st.write(article_temp.format(i[1],i[0],i[3],i[2]),unsafe_allow_html=True)
-                st.write(head_message_temp.format(i[1], i[0], i[3]), unsafe_allow_html=True)
-                st.write(full_message_temp.format(i[2]), unsafe_allow_html=True)
-
-
-    elif choice == "Manage Blog":
-        st.subheader("Manage Blog")
-        result = view_all_notes()
-        clean_db = pd.DataFrame(result, columns=["Author", "Title", "Article", "Date", "Index"])
-        st.dataframe(clean_db)
-        unique_list = [i[0] for i in view_all_titles()]
-        delete_by_title = st.selectbox("Select Title", unique_list)
-        if st.button("Delete"):
-            delete_data(delete_by_title)
-            st.warning("Deleted: '{}'".format(delete_by_title))
-
-        if st.checkbox("Metrics"):
-            new_df = clean_db
-            new_df['Length'] = new_df['Article'].str.len()
-
-            st.dataframe(new_df)
-            # st.dataframe(new_df['Author'].value_counts())
-            st.subheader("Author Stats")
-            new_df['Author'].value_counts().plot(kind='bar')
-            st.pyplot()
-
-            new_df['Author'].value_counts().plot.pie(autopct="%1.1f%%")
-            st.pyplot()
-
-        if st.checkbox("WordCloud"):
-            # text = clean_db['Article'].iloc[0]
-            st.subheader("Word Cloud")
-            text = ', '.join(clean_db['Article'])
-            # Create and generate a word cloud image:
-            wordcloud = WordCloud().generate(text)
-
-            # Display the generated image:
-            plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis("off")
-            st.pyplot()
-
-        if st.checkbox("BarH Plot"):
-            st.subheader("Length of Articles")
-            new_df = clean_db
-            new_df['Length'] = new_df['Article'].str.len()
-            barh_plot = new_df.plot.barh(x='Author', y='Length', figsize=(10, 10))
-            st.write(barh_plot)
-            st.pyplot()
 
 
 if __name__ == '__main__':
