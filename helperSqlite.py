780021deed8e2f2e4fbc59bd92b8c7e01ed60b3e
alinedantas <alinepinheiro20@gmail.com>
2022-02-16 17:47:32 -0300

    Add files via upload

--- helperSqlite.py
+++ helperSqlite.py
@@ -1,22 +1,36 @@
+# DB
 import sqlite3
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+# Functions
+def create_table():
+	c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT,title TEXT,article TEXT,postdate DATE)')
+
+def add_data(author,title,article,postdate):
+	c.execute('INSERT INTO blogtable(author,title,article,postdate) VALUES (?,?,?,?)',(author,title,article,postdate))
+	conn.commit()
+
+def view_all_notes():
+	c.execute('SELECT * FROM blogtable')
+	data = c.fetchall()
+	return data
+
+def view_all_titles():
+	c.execute('SELECT DISTINCT title FROM blogtable')
+	data = c.fetchall()
+	return data
 
 
-def ping():
-    print('working ..')
+def get_blog_by_title(title):
+	c.execute('SELECT * FROM blogtable WHERE title="{}"'.format(title))
+	data = c.fetchall()
+	return data
+def get_blog_by_author(author):
+	c.execute('SELECT * FROM blogtable WHERE author="{}"'.format(author))
+	data = c.fetchall()
+	return data
 
-
-def getAllData(sqlString, path):
-    db = sqlite3.connect(path, uri=True)
-    cur = db.cursor()
-    cur.execute(sqlString)
-    return cur.fetchall()
-
-
-def crud(sqlString, path):
-    db = sqlite3.connect(path)
-    c = db.cursor()
-    c.execute(sqlString)
-    db.commit()
-    db.close()
-
-    # st.title("display graph")
+def delete_data(title):
+	c.execute('DELETE FROM blogtable WHERE title="{}"'.format(title))
+	conn.commit()
